121056f254ec8abb517ba5438e0a53b6f76d6745
Tim <your_email@example.com>
2026-09-24 11:34:33 +0400

    Fix Docker build

--- config.php
+++ config.php
@@ -12,7 +12,7 @@ return [
     | BITRIX_HOOK
     |
     */
-    'BITRIX_HOOK' => getenv('https://b24-ef9noe.bitrix24.ru/rest/1/55yppqh1b90kq575/') ?: '',
+    'BITRIX_HOOK' => 'https://b24-ef9noe.bitrix24.ru/rest/1/55yppqh1b90kq575/',
 
     /*
     |--------------------------------------------------------------------------
